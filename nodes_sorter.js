61df462694bb8bf491faa9ce6e715c99931f019d
Lion <44504201+PickkName@users.noreply.github.com>
2026-03-17 23:13:19 +0800

    Refactor node sorting to include Japan and Taiwan

--- nodes_sorter.js
+++ nodes_sorter.js
@@ -1,5 +1,5 @@
 /**
-功能：将"服务有效"节点排最前，香港/台湾节点其次，其他节点最后
+功能：将"服务有效"节点排最前，然后依次是香港、日本、台湾，其他节点最后
 
 在订阅链接后面添加任意参数即可触发解析器（比如 #sort=1）
 */
@@ -10,9 +10,11 @@ let content = $resource.content;
 // 按行分割
 let lines = content.split('\n');
 
-// 分类：三个优先级
+// 分类：五个优先级
 let serviceNodes = []; // 服务有效节点（最高优先级）
-let hktwNodes = [];    // 香港台湾节点
+let hkNodes = [];      // 香港节点
+let jpNodes = [];      // 日本节点
+let twNodes = [];      // 台湾节点
 let otherNodes = [];   // 其他节点
 
 lines.forEach(line => {
@@ -25,16 +27,20 @@ lines.forEach(line => {
     if (line.includes('服务有效')) {
         serviceNodes.push(line);
     }
-    // 判断是否包含"香港"或"台湾"
-    else if (line.includes('香港') || line.includes('台湾')) {
-        hktwNodes.push(line);
+    // 判断地区优先级：香港 > 日本 > 台湾
+    else if (line.includes('香港')) {
+        hkNodes.push(line);
+    } else if (line.includes('日本')) {
+        jpNodes.push(line);
+    } else if (line.includes('台湾')) {
+        twNodes.push(line);
     } else {
         otherNodes.push(line);
     }
 });
 
-// 合并：服务有效在最前，香港台湾其次，其他在后
-let sortedContent = [...serviceNodes, ...hktwNodes, ...otherNodes].join('\n');
+// 合并：服务有效在最前，然后是香港、日本、台湾，其他在后
+let sortedContent = [...serviceNodes, ...hkNodes, ...jpNodes, ...twNodes, ...otherNodes].join('\n');
 
 // 返回处理后的内容
 $done({ content: sortedContent });
